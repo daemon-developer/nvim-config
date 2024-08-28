031b2c2c45c60ff9622a5b5145237705229b5f37
Mark <mark@markallen.mobi>
2024-08-28 11:16:57 +0100

    Some extra options

--- init.lua
+++ init.lua
@@ -1,7 +1,25 @@
 require("config.lazy")
 
--- Set up tab widths of 2 spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.termguicolors = true
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
 
